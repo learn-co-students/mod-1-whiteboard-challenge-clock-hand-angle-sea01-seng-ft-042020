b78e19eb488e4438df2c1cd3cbbc0bc85b71c439
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-05-06 23:25:34 -0700

    Done.

--- lib/solution.rb
+++ lib/solution.rb
@@ -1,3 +1,7 @@
 def clock_angle(time)
-    # code your solution here
+    time = time.split(":")
+    # 60 minutes per hour
+    minutes = time.first.to_f * 60 + time.last.to_f
+    # 720 minutes per day, 360 degrees per rotation therefore conversion factor of 0.5
+    angle = (720 - minutes)/2
 end
